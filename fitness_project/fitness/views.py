b047254de2d110a511108c89175a92bd73166ece
Pontaaaa <>
2024-01-28 14:45:49 +0000

    add authentication paths

--- fitness_project/fitness/views.py
+++ fitness_project/fitness/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
