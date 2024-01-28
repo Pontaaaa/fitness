0d68e6d4259935106202bc3d2eb4e614e1f3ff1c
Pontaaaa <>
2024-01-28 14:22:39 +0000

    add models for rating and subscription

--- fitness_project/fitness/models.py
+++ fitness_project/fitness/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class WorkoutPlan(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -9,3 +10,21 @@ class WorkoutPlan(models.Model):
     def __str__(self):
         return self.title
 
+
+class Subscription(models.Model):
+    user = models.ForeignKey(WorkoutPlan, on_delete=models.CASCADE)
+    workout_plan = models.ForeignKey(WorkoutPlan, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.workout_plan.title}"
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workout_plan = models.ForeignKey(WorkoutPlan, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.workout_plan.title} - {self.rating}"
